9e43f58f1b01d056d5b0d8c605a3e4b85f20e07f
Artem Ruzaev <ruzaev.artem21@gmail.com>
2025-07-03 22:59:27 -0400

    Add delete and export routes

--- app.rb
+++ app.rb
@@ -29,3 +29,25 @@ get "/entries" do
   @entries = File.exist?("log.txt") ? File.readlines("log.txt") : []
   erb :entries
 end
+
+post "/delete" do
+  index = params[:index].to_i
+  entries = File.exist?("log.txt") ? File.readlines("log.txt") : []
+
+  if index >= 0 && index < entries.length
+    # entries are displayed in reverse order, so delete from the end
+    entries.delete_at(-1 - index)
+    File.open("log.txt", "w") { |f| f.puts(entries) }
+  end
+
+  redirect "/entries"
+end
+
+get "/export" do
+  if File.exist?("log.txt")
+    send_file "log.txt", filename: "log.txt", type: "text/plain", disposition: "attachment"
+  else
+    status 404
+    "No log available"
+  end
+end
